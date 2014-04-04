d71a1dfb3a8cbeb4c5eb0466eae0bcf27f689e5c
Simon Newton <nomis52@gmail.com>
2014-04-04 20:31:17 +0800

    Another update to update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -x
 repo=$(dirname $0)
 cd $repo
 git fetch origin
 output=$(git log HEAD..origin/master --oneline)
 if [ -n "$output" ]; then
   echo "Merging";
+  set -x
   git merge origin/master;
   buildbot checkconfig master.cfg
   # checkconfig exits 1 if everything is ok.
