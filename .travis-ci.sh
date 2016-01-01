7e589686535ffa680d975339a102dc5871427b67
Peter Newman <peternewman@users.noreply.github.com>
2016-01-01 15:34:52 +0000

    Try and fix Travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -8,7 +8,7 @@ if [[ $TASK = 'flake8' ]]; then
   # build.config has to be checked seperately, as it's not strictly a Python file
   flake8 --max-line-length 80 --exclude *_pb2.py,.git,__pycache --ignore E111,E121,E129,F821 build.config
 elif [[ $TASK = 'pychecker' ]]; then
-  pychecker --no-import --quiet build.config master.cfg 
+  # Can't check these files as they aren't true .py files build.config master.cfg 
   pychecker --quiet --maxargs 12 config_helper.py pass_toucher.py
 else
   # Otherwise run checkconfig as normal
